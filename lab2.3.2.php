ea62fe73785c33819a31612749aa85650bdd0883
kajalsharma127265-dotcom <kajal.sharma127265@marwadiuniversity.ac.in>
2026-08-19 21:17:46 +0530

    Add files via upload

---
+++ lab2.3.2.php
@@ -0,0 +1,11 @@
+<?php
+$arr = array("Red", "Green", "Blue", "Yellow");
+
+echo "Original Array:<br>";
+print_r($arr);
+
+array_pop($arr);
+
+echo "<br><br>After array_pop():<br>";
+print_r($arr);
+?>
