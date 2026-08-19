e37eb626e468c70a8796abc3fc28fb82667b48b9
kajalsharma127265-dotcom <kajal.sharma127265@marwadiuniversity.ac.in>
2026-08-19 21:24:24 +0530

    Add files via upload

---
+++ extra.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Form</title>
+</head>
+<body>
+
+    <h2>Enter Username</h2>
+
+    <form action="display.php" method="post">
+        <label>Username:</label>
+        <input type="text" name="username" required>
+        <button type="submit">Submit</button>
+    </form>
+
+</body>
+</html>
